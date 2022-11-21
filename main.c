afff8825b0ae6e29d429c6bac65f8acaa829c5d5
Misha <20xim04@gmail.com>
2022-11-21 14:10:11 +0200

    timer

--- main.c
+++ main.c
@@ -1,11 +1,20 @@
 #include"client.h"
+#include"time.h"
 #include"olxclient.h"
-void AUTH(REQ_status_t* status){
+clock_t start=0;
+void AUTH(REQ_status_t* status,void* data){
+    static int i=0;
     if(status->code==1){
-        printf("\nAUTH SERVER\n");
+        printf("\nAUTH SERVER %d\n",i);
+        if(i==999){
+            clock_t end=clock();
+            double time_in_seconds = (double)(end - start) / CLOCKS_PER_SEC;
+            printf("%lf\n",time_in_seconds);
+        }
     }else{
         printf("ERROR AUTH\n");
     }
+    i++;
 }
 int main(){
    
@@ -14,16 +23,16 @@ int main(){
     cl_InitClient(&client);
     InitREQpack(&client);
     cl_ClientConnect(&client,"192.168.0.105",9999);
+
     RES_auth_t auth;
     CreateRES_auth(&auth);
-    printf("INPUT NAME: ");
-    scanf("%s",auth.name);
-    printf("INPUT PASSWORD: ");
-    scanf("%s",auth.password);
+    strcpy(auth.name,"misha");
+    strcpy(auth.password,"123");
+    start=clock();
+    for(int i=0;i<1000;i++){
+        cl_SendPack(&client,&auth,AUTH,NULL);
+    }
     
-    cl_SendPack(&client,&auth,AUTH);
-
-    printf("END\n");
     int num=0;
     scanf("%d",&num);
     return 0;
