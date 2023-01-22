dd76fb5178d698beea2fe270907358b3604f54f4
Misha <20xim04@gmail.com>
2023-01-22 14:55:40 +0200

    edit

--- include/network.h
+++ include/network.h
@@ -1,33 +1,44 @@
 #pragma once
-#include"client.h"
+#include "client.h"
 using namespace client;
 extern Client cl;
+class RES_auth: public pack_res{
+  private:
+  std::string name;
+  std::string password;
+  public:
+  RES_auth(){
+    idpack=1;
+  }
+  pars::json_construct GetJsonPack() override{
+    pars::json_construct json;
+    pars::json_item name;
+    name.init("name",this->name);
+    pars::json_item password;
+    password.init("password",this->password);
+    json.addelement(name);
+    json.addelement(password);
+    return json;
+  }
+};
 
-// typedef struct RES_auth{
-//     cl_packres_t pack;
-//     char name[100];
-//     char password[20];
-// }RES_auth_t;
-// void CreateRES_auth(RES_auth_t* auth);
-// cl_json_construct_t RES_auth_getjson(RES_auth_t* auth);
-class REQ_status:public pack{
-private:
-    int code;
-public:
-    bool JsonToData(json_value* json){
-        json_object_entry* jcode=pars::jsonGetNameKey(json,"code");
-        if(jcode!=NULL&&jcode->value->type==json_integer){
-            code=jcode->value->u.integer;
-            return true;
-        }
-        return false;
+class REQ_status : public pack_req {
+ private:
+  int code;
+
+ public:
+  bool JsonToData(json_value* json) override{
+    json_object_entry* jcode = pars::jsonGetNameKey(json, "code");
+    if (jcode != NULL && jcode->value->type == json_integer) {
+      code = jcode->value->u.integer;
+      return true;
     }
-    void ProcessPack(){
-        printf("STATUS: %d\n",code);
-    }
-    void InitPack(){
-        idpack=10;
-        code=0;
-    }
+    return false;
+  }
+  void ProcessPack() override { printf("STATUS: %d\n", code); }
+  void InitPack() override{
+    idpack = 10;
+    code = 0;
+  }
 };
 void ConnectMainServer();
--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
-#include"client.h"
-#include"time.h"
-#include"olxclient.h"
-int main(){
-    printf("HELLO CLIENT START\n");
-    try{
-        ConnectMainServer();
-    }catch(NetworkExeption& ntw){
-        std::cout<<ntw.what();
-    }
-    while(true){
-        
-    }
-    return 0;
+#include "client.h"
+#include "olxclient.h"
+#include "time.h"
+int main() {
+  printf("HELLO CLIENT START\n");
+  try {
+    ConnectMainServer();
+    
+  } catch (NetworkExeption& ntw) {
+    std::cout << ntw.what();
+  }
+  while (true) {
+  }
+  return 0;
 }
